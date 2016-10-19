a6f293747ffa8057098c1b739648897a7d2777c4
Dima <dima.buchka@gmail.com>
2016-10-19 02:42:49 -0400

    Start of the Foundation Contd.
    -Clouds is a class now.
    -For convenience, you can move through the 'world' container with the
    mouse now.

--- script/classes.js
+++ script/classes.js
@@ -1,6 +1,6 @@
 (function(window){
-    function World(ground){
-        this.initialize(ground);
+    function World(){
+        this.initialize();
     }
     World.prototype = new createjs.Container();
 
@@ -14,11 +14,12 @@
         this.height = settings.height;
         this.x = 0;
         this.y = -3000;
-        //ground.x = 0;
-        //ground.y = 4500;
+        this.velX = 0;
+        this.velY = 0;
     }
     World.prototype.tick = function(){
-        this.y--;
+        this.y += this.velY;
+        this.x += this.velX;
         if(this.x<(canvas.width-this.width)) this.x = canvas.width-this.width;
         if(this.x>0) this.x = 0;
         if(this.y<(canvas.height-this.height)) this.y = canvas.height-this.height;
@@ -26,3 +27,61 @@
     }
     window.World = World;
 } (window));
+
+(function(window){
+    function Ground(groundShape){
+        this.initialize(groundShape);
+    }
+    Ground.prototype = new createjs.Container();
+
+    Ground.prototype.container_initialize = Ground.prototype.initialize;
+
+    Ground.prototype.initialize = function(groundShape) {
+        this.container_initialize();
+        this.name = 'ground';
+        this.setBounds(0, settings.height-settings.ground.height, settings.width, settings.ground.height);
+        this.x = 0;
+        this.y = settings.height-settings.ground.height;
+        this.width = settings.width;
+        this.height = settings.ground.height;
+        this.addChild(groundShape);
+    }
+    Ground.prototype.tick = function(){
+    }
+    window.Ground = Ground;
+} (window));
+
+(function(window){
+    function Cloud(){
+        this.initialize();
+    }
+    Cloud.prototype = new createjs.Container();
+
+    Cloud.prototype.container_initialize = Cloud.prototype.initialize;
+
+    Cloud.prototype.initialize = function() {
+        this.container_initialize();
+        this.name = 'cloud';
+        this.setBounds(getRand(-settings.cloud.width+1, settings.width+settings.cloud.width-1), getRand(settings.cloud.minHeight, settings.cloud.MaxHeight), settings.cloud.width, settings.cloud.height);
+        this.width = settings.cloud.width;
+        this.height = settings.cloud.height;
+        this.x = getRand(-settings.cloud.width+1, settings.width+settings.cloud.width-1);
+        this.y = getRand(settings.cloud.minHeight, settings.cloud.MaxHeight);
+        this.vel = getRand(settings.cloud.minVel, settings.cloud.maxVel);
+        var cloudBitmap = new createjs.Bitmap(assets.cloudImage[getRand(0,3)]);
+        //cloudBitmap.x = 0;
+        //cloudBitmap.y = 0;
+        this.addChild(cloudBitmap);
+    }
+    Cloud.prototype.tick = function(){
+        this.x-=this.vel;
+        if(this.x<(-settings.cloud.width)){
+            stage.getChildByName('world').removeChild(this);
+            settings.cloud.count--;
+        }
+    }
+    window.Cloud = Cloud;
+} (window));
+function getRand(min, max){
+    return Math.floor(Math.random()*(max-min)+min);
+}
--- script/index.js
+++ script/index.js
@@ -13,11 +13,19 @@ settings.width = 5000;
 settings.height = 4500;
 settings.ground.width = settings.width;
 settings.ground.height = 500;
+settings.cloud.width = 1000;
+settings.cloud.height = 300;
 settings.cloud.minVel = 0;
 settings.cloud.maxVel = 5;
 settings.cloud.minHeight = settings.height - settings.ground.height - 1000;
-settings.cloud.maxHeight = settings.height + 500;
+settings.cloud.maxHeight = -300;
+settings.cloud.maxCount = 100;
+settings.cloud.count = 0;
 //----End of settings----//
+
+//----Assets are to keep the objects you need at hand----//
+//----For more information, refer to the mind map----//
+var assets = new Object();
 
 var groundImage = new Image(),
     atmosphereImage = new Image(),
@@ -25,19 +33,18 @@ var groundImage = new Image(),
     cloudImage2 = new Image(),
     cloudImage3 = new Image(),
     cloudImage4 = new Image();
-    //world = new createjs.Container();
 function init(){
     fixCanvas();
     loadAssets();
 
     stage = new createjs.Stage('canvas');
     var world = new World();
-    //world.setBounds(0, 0, settings.width, settings.height);
-    //world.x = 0;
-    //world.y = -3000;
-    //world.name = 'world';
     stage.addChild(world);
-
+    
+    stage.on('stagemousemove', function(e){
+        stage.getChildByName('world').velX = (canvas.width/2 - e.rawX)/4;
+        stage.getChildByName('world').velY = (canvas.height/2 - e.rawY)/4;
+    });
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener('tick', tick);
     function tick(e){
@@ -46,35 +53,63 @@ function init(){
     }
 }
 function loadAssets(){
-    groundImage.onload = onGroundImageLoad;
-    atmosphereImage.onload = onAtmosphereImageLoad;
-    cloudImage1.onload = onCloudImageLoad(cloudImage1);
-    groundImage.src = "./img/world_ground.png";
-    atmosphereImage.src = "./img/world_atmosphere.png";
-    cloudImage1.src = './img/world_cloud1.png';
+    assets.groundImage = new Image();
+    assets.atmosphereImage = new Image();
+    assets.cloudImage = [];
+    assets.cloudImage[0] = new Image();
+    assets.cloudImage[1] = new Image();
+    assets.cloudImage[2] = new Image();
+    assets.cloudImage[3] = new Image();
+    assets.groundImage.onload = onGroundImageLoad;
+    assets.atmosphereImage.onload = onAtmosphereImageLoad;
+    assets.cloudImage[0].onload = onCloudImageLoad;
+    assets.cloudImage[1].onload = onCloudImageLoad;
+    assets.cloudImage[2].onload = onCloudImageLoad;
+    assets.cloudImage[3].onload = onCloudImageLoad;
+    assets.cloudImage[0].isLoaded = false;
+    assets.cloudImage[1].isLoaded = false;
+    assets.cloudImage[2].isLoaded = false;
+    assets.cloudImage[3].isLoaded = false;
+    assets.groundImage.src = "./img/world_ground.png";
+    assets.atmosphereImage.src = "./img/world_atmosphere.png";
+    assets.cloudImage[0].src = './img/world_cloud1.png';
+    assets.cloudImage[1].src = './img/world_cloud2.png';
+    assets.cloudImage[2].src = './img/world_cloud3.png';
+    assets.cloudImage[3].src = './img/world_cloud4.png';
 }
 function onGroundImageLoad(e){
     var groundShape = new createjs.Shape();
     groundShape.width = settings.ground.width;
     groundShape.height = settings.ground.height;
     groundShape.graphics = new createjs.Graphics();
-    groundShape.graphics.beginBitmapFill(groundImage);
-    groundShape.graphics.drawRect(0, settings.height-settings.ground.height, settings.ground.width, settings.ground.height);
+    groundShape.graphics.beginBitmapFill(this);
+    groundShape.graphics.drawRect(0, 0, settings.ground.width, settings.ground.height);
     
-    stage.getChildByName('world').addChild(groundShape);
+    var ground = new Ground(groundShape);
+    stage.getChildByName('world').addChild(ground);
     stage.update();
 }
 function onAtmosphereImageLoad(e){
     var atmosphereShape = new createjs.Shape();
     atmosphereShape.graphics = new createjs.Graphics();
-    atmosphereShape.graphics.beginBitmapFill(atmosphereImage);
+    atmosphereShape.graphics.beginBitmapFill(this);
     atmosphereShape.graphics.drawRect(0, 0, settings.width, settings.height-settings.ground.height);
+    atmosphereShape.name = 'atmosphere';
     
     stage.getChildByName('world').addChild(atmosphereShape);
     stage.update();
     
 }
-function onCloudImageLoad(e, cloudImage){
-    var cloudBitmap = new createjs.Bitmap(cloudImage);
-    //cloudBitmap.x = Math.floor(Math.random()*(stage.getChildByName('world').width) );
+function onCloudImageLoad(e){
+    this.isLoaded = true;
+    if(assets.cloudImage[0].isLoaded && assets.cloudImage[1].isLoaded && assets.cloudImage[2].isLoaded && assets.cloudImage[3].isLoaded) initializeClouds();
+}
+function initializeClouds(){
+    for(var i=0; i<settings.cloud.maxCount; i++){
+        var cloud = new Cloud();
+        stage.getChildByName('world').addChild(cloud);
+    }
+}
+function onMouseMove(e){
+    //stage.getChildByName('world').vel.x = e.rawX - canvas.width/
 }
