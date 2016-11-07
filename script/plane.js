bce811e52f2c9ad8302b99debf561fc05ab181a1
Dima <dima.buchka@gmail.com>
2016-11-06 21:30:01 -0500

    Plane Cont'd
    -We have the second takeoff!
    -This is a cornerstone as no the plane behaves as it should. Besides the
    gravity and the airdrag. Also, when in air and the engine is cut, the
    airdrag implemented so far, suspends the plane in the air haulting its
    motion.
    +add Gravity
    +add Airdrag

--- script/plane.js
+++ script/plane.js
@@ -20,6 +20,7 @@
         this.velX = 0;
         this.velY = -0;
         this.velRotation = 0;
+        this.cursorRotation = 0;
         this.engineOn = false;
         this.gravity = {
             y : 0,
@@ -34,22 +35,32 @@
     }
     Plane.prototype.tick = function(){
         //----Rotating the plane----//
-        this.rotation = -Math.atan2(assets.mouse.x - this.x-assets.world.x, assets.mouse.y - this.y-assets.world.y)*180/Math.PI+90;
-        if(this.vel>1){
-                this.velRotation = this.velRotation + ((this.rotation-this.velRotation)*((settings.plane.maxVel-this.vel)/settings.plane.maxVel));
-                console.log('vel: ' + ((settings.plane.maxVel-this.vel)/settings.plane.maxVel) + '\nrotation: ' + this.rotation + '\nvelRotation: ' + this.velRotation);
+        this.cursorRotation = -Math.atan2(assets.mouse.x - this.x-assets.world.x, assets.mouse.y - this.y-assets.world.y)*180/Math.PI+90;
+        
+        if(Math.abs(this.cursorRotation-this.velRotation)>180){
+            if(this.cursorRotation>this.velRotation) this.velRotation+=360;
+            else this.velRotation-=360;
         }
+        if(this.vel>1){
+            this.velRotation = (this.velRotation + ((this.cursorRotation-this.velRotation)*(this.vel/(settings.plane.maxVel+200))));
+             console.log('+vel: ' + this.vel + '\nrotation: ' + this.cursorRotation + '\nvelRotation: ' + this.velRotation);
+        } else {
+            this.velRotation = (this.velRotation + ((this.cursorRotation-this.velRotation)*(1/settings.plane.maxVel)));
+            console.log('-vel: ' + this.vel + '\nrotation: ' + this.cursorRotation + '\nvelRotation: ' + this.velRotation);
+        }
+        this.rotation = this.velRotation;
         //----Moving the plane----//
         //Vector velocity
         if(this.vel<settings.plane.maxVel && this.engineOn){
-            this.vel+=/*((Math.cos(this.rotation * (Math.PI/180))) * */0.3//);
+            this.vel+=/*((Math.cos(this.cursorRotation * (Math.PI/180))) * */0.3//);
         }
+        else if(this.vel>0) this.vel-=0.5;
         //Velocity Y
         this.velY = ((Math.sin(this.velRotation * (Math.PI/180))) * this.vel);
         if(this.y+this.height<settings.height-settings.ground.height){
-            if(this.vel<2){
+            //if(this.vel<2){
                 this.velY += (this.vel<1) ? this.velY : 1;
-            }
+            //}
         }
         else if(this.velY>0){
             this.velY = 0;
